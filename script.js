e891a2016db218db22d6cb9c735899652983c0d4
bheemreddysakethreddy <sakethbheem@gmail.com>
2025-10-27 15:31:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
  let prices=document.querySelectorAll('.price')
- let sumarr = Array.from(prices)
-sumarr=sumarr.map(Number)
- let total =  sumarr.reduce((acc,sum)=>sum+acc,0)
+ let total = 0 
+prices.forEach((p)=>{
+	total+=parseInt(p.innerText)
+})
  const totalrow = document.createElement('tr');
  const totalname = document.createElement("td");
  const totalprice = document.createElement('td');
 totalname.textContent="total Price"
-totalprice.textContent=410
+totalprice.textContent=`${total}`
 totalrow.append(totalname,totalprice)
 totalrow.id="ans"
 document.querySelector("table").appendChild(totalrow);
